4d93d8db623103fdbce15453572d7e019347b88c
Gor-Madoyan <gor.madoyan98@gmail.com>
2021-07-18 11:56:33 +0400

    my last update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,55 @@
 import { Component } from '@angular/core';
-import { Ranger } from "src/app/rangers-buttons/logic"
+import { fightLogic, Ranger, fights } from "src/app/rangers-buttons/logic"
  @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'fight-rangers';
+  
+  redrangerName:any;
+  redrangerpower:any;
+
+  bluerangerName:any;
+  bluerangerpower:any;
+
+  objRangers = new fights("Blue");
 
   rangerRed(){
     const redRenger = new Ranger('Red')
-    redRenger.ranger1()
-    redRenger.fight();
+    redRenger.rangersConfiguration()
+    this.redrangerName = redRenger.RedrangerName
+    this.redrangerpower = redRenger.Redrangerpower
     
-  }
+  };
   rangerBlue(){
     const bluedRenger = new Ranger('Blue');
-    bluedRenger.ranger2()
-  }
-  
-  
+    bluedRenger.rangersConfiguration()
+    this.bluerangerName = bluedRenger.RedrangerName
+    this.bluerangerpower = bluedRenger.Redrangerpower
+  };
+
+rangerFights() {
+  this.objRangers.blueRangerFight()
+  this.objRangers.redRangerFight()
+};
+
+stopFight() {
+  setTimeout(()=>{
+    clearInterval(this.objRangers.timeoutRed);
+    clearInterval(this.objRangers.timeoutBlue);
+
+    if(this.objRangers.redLastNumber < this.objRangers.blueLastNumber) {
+      console.log(`blue won  ${this.objRangers.blueLastNumber}%`);
+
+    }else if(this.objRangers.redLastNumber === this.objRangers.blueLastNumber) {
+
+      console.log('drow');
+    }else  {
+      console.log(`red won ${this.objRangers.redLastNumber}%`);
+    }   
+  }, 3000)
+
+}
 
 }
--- src/app/rangers-buttons/logic.ts
+++ src/app/rangers-buttons/logic.ts
@@ -1,28 +1,75 @@
+import { stringify } from "@angular/compiler/src/util";
+
 export class Ranger{
     name:string;
-    power:number
+    power:number;
     constructor(name:string) {
-      this.name = name
+      this.name = name;
       this.power = 100
      };
 
-     ranger1() {
-        console.log('ranger1')    
+     RedrangerName:any;
+     Redrangerpower:any;
+   
+     rangersConfiguration () {
+        this.RedrangerName = `Ranger name - ${this.name} `
+        this.Redrangerpower = `Ranger power - ${this.power}%`
       }
-      ranger2() {
-          console.log('ranger2')
-          
-        }
+    
+};
 
-        fight() {
-           this.power = this.power - Math.trunc(Math.random()*100)
-           if(this.power <= 0) {
-               console.log('you lose');
-               
-           }
-           console.log(this.power);
-           
-        }
+export class fightLogic extends Ranger{ 
+    constructor(name:string) {
+        super(name)
+    };
+
+    blueLastNumber:number = 100;
+    blueRandomNumber() {
+    this.blueLastNumber = this.blueLastNumber - Math.trunc( Math.random() * 5)
+    };
+    
+    redLastNumber:number = 100;
+    redRandomNuber() {
+      this.redLastNumber = this.redLastNumber - Math.trunc( Math.random() * 5)
+    };
 }
 
-const redRenger = new Ranger('Red')
+
+export class fights extends fightLogic {
+
+  constructor(name:string) {
+    super(name)
+  };
+
+  timeoutBlue:any;
+  timeoutRed:any;
+
+  blueRangerFight() {
+    this.timeoutBlue = setInterval(()=>{
+      this.blueRandomNumber();
+      if(this.blueLastNumber > 0) {
+        console.log(this.blueLastNumber, 'blue ranger');
+      }else {
+        clearInterval(this.timeoutBlue);
+        clearInterval(this.timeoutRed);
+        alert(`Blue Ranger lose`)
+      }
+    }, 500)
+  };
+
+  redRangerFight() {
+    this.timeoutRed = setInterval(()=>{
+     this.redRandomNuber();
+     if(this.redLastNumber > 0) {
+       console.log(this.redLastNumber, 'red ranger');
+     }else {
+        clearInterval(this.timeoutRed);
+        clearInterval(this.timeoutBlue);
+        alert(`Red Ranger lose`)        
+        }
+    }, 500);
+  }
+}
+
+
+
